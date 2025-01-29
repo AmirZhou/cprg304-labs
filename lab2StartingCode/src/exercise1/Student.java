5c31ffbe43a9940328f8e02bec13dc3bd404bd92
Amir Yue Zhou <amirzhou001@gmail.com>
2025-01-29 16:53:11 -0700

    feat: lab2 exercise1

--- lab2StartingCode/src/exercise1/Student.java
+++ lab2StartingCode/src/exercise1/Student.java
@@ -1,13 +1,34 @@
 package exercise1;
 
-public class Student
-{
+
+/**
+ * @author Yue (Amir) Zhou
+ *  
+ */
+public class Student implements Comparable<Student>{
 	private final String name;
 	private final int age;
 	
-	public Student( String name, int age )
-	{
+	public int getAge() {
+		return age;
+	};
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Student( String name, int age ) {
 		this.name = name;
 		this.age = age;
+	}
+	
+	@Override
+	public int compareTo(Student o) {
+		return Integer.compare(this.age, o.getAge());
+	}
+	
+	@Override
+	public String toString() {
+		return this.getName() + ':' + this.getAge();
 	}
 }
